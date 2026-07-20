3f3e28831d4f1a0b9e7d1467aeddd4c9c50ffbe7
Karim <kelkhaterk@gmail.com>
2026-07-20 20:46:08 +0300

    Added parameterised XOR cipher RTL

---
+++ rtl/xor_cipher.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 07/20/2026 08:24:47 PM
+// Design Name: 
+// Module Name: xor_cipher
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module xor_cipher(
+
+    );
+endmodule

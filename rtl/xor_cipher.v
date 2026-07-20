7889f8b7cf3518c8a9718fd7f348f8982424d2b1
Karim <kelkhaterk@gmail.com>
2026-07-20 20:58:56 +0300

    XOR Cipher first iteration

--- rtl/xor_cipher.v
+++ rtl/xor_cipher.v
@@ -1,26 +1,11 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 07/20/2026 08:24:47 PM
-// Design Name: 
-// Module Name: xor_cipher
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
+module xor_cipher #(parameter WIDTH = 8)(
+input [WIDTH - 1 : 0] key,
+input [WIDTH - 1 : 0] data_in,
+output [WIDTH - 1: 0] data_out
+);
 
 
-module xor_cipher(
+assign data_out = key ^ data_in;
 
-    );
 endmodule
